d150a10521b359ccfdf91191779fdca1e5e4cbf4
Bárbara Romero <bqromero@gmail.com>
2021-09-28 18:16:56 -0300

    Criar método menu na classe sistema

--- src/br/com/zup/Sistema.java
+++ src/br/com/zup/Sistema.java
@@ -1,4 +1,13 @@
 package br.com.zup;
 
 public class Sistema {
+  public static void menu(){
+    System.out.println("\n*** RESTAURANTE COMIDAS DE VÓ ***");
+    System.out.println("\n\t\t\t M E N U");
+    System.out.println("\n1 - Adicionar novo prato");
+    System.out.println("2 - Adicionar novo ingrediente");
+    System.out.println("3 - Exibir cardápio");
+    System.out.println("4 - Voltar ao menu principal");
+    System.out.println("5 - Sair");
+  }
 }
